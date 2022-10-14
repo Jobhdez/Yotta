88c713724873d8af5679e4f508742a5eb501ebd1
Job <hj93@protonmail.com>
2022-10-13 20:28:04 -0700

    add node structures.

--- src/parser/parser.lisp
+++ src/parser/parser.lisp
@@ -10,6 +10,51 @@
     (declare (ignore a c))
     b)
 
+  (defstruct plus left-exp right-exp)
+
+  (defstruct minus left-exp right-exp)
+
+  (defstruct mul left-exp right-exp)
+  
+  (defstruct vec entries)
+
+  (defstruct matrix entries)
+
+  (defstruct num num)
+
+  (defstruct assignment variable exp)
+  
+  (defun build-plus (left-exp plus-tok right-exp)
+    (declare (ignore plus-tok))
+    (make-plus :left-exp left-exp
+	       :right-exp right-exp))
+
+  (defun build-minus (left-exp minus-tok right-exp)
+    (declare (ignore minus-tok))
+    (make-minus :left-exp left-exp
+		:right-exp right-exp))
+
+  (defun build-mul (left-exp mul-tok right-exp)
+    (declare (ignore mul-tok))
+    (make-mul :left-exp left-exp
+	      :right-exp right-exp))
+
+  (defun build-vec (left-bracket entries right-bracket)
+    (declare (ignore left-bracket right-bracket))
+    (make-vec :entries (flatten entries)))
+
+  (defun build-matrix (left-bracket entries right-bracket)
+    (declare (ignore left-bracket right-bracket))
+    (make-matrix :entries entries))
+
+  (defun build-num (num)
+    (make-num :num num))
+
+  (defun build-assignment (var assignment exp)
+    (declare (ignore assignment))
+    (make-assignment :variable var
+		     :exp exp))
+
   (define-parser *linear-algebra-grammar*
       (:start-symbol expression)
     (:terminals (:number :variable :plus :minus :mul :left-bracket :right-bracket :assignment))
@@ -17,15 +62,15 @@
      vector-exp
      matrix-exp
      assignment
-     (expression :plus expression #'infix-to-prefix)
-     (expression :minus expression #'infix-to-prefix)
-     (expression :mul expression #'infix-to-prefix))
+     (expression :plus expression #'build-plus)
+     (expression :minus expression #'build-minus)
+     (expression :mul expression #'build-mul))
     (vector-exp
      (:left-bracket :right-bracket)
-     (:left-bracket entries :right-bracket))
+     (:left-bracket entries :right-bracket #'build-vec))
     (matrix-exp
      (:left-bracket  :right-bracket)
-     (:left-bracket vectors :right-bracket))
+     (:left-bracket vectors :right-bracket #'build-matrix))
     (vectors
      vector-exp
      (vector-exp vectors))
@@ -33,9 +78,9 @@
      scalar-exp
      (scalar-exp entries))
     (scalar-exp
-     :number)
+     (:number #'build-num))
     (assignment
-     (:variable :assignment expression #'infix-to-prefix))))
+     (:variable :assignment expression #'build-assignment))))
   
 (defun token-generator (toks)
  "Make a lexer that is compatible with CL-YACC."
