0fe28aa3b9162279cc689ea380ca67a0ab167dc1
Job <hj93@protonmail.com>
2022-10-19 20:06:35 -0700

    export functions.

--- src/package.lisp
+++ src/package.lisp
@@ -5,4 +5,25 @@
 	#:alexandria
 	#:trivia)
   (:export #:token-generator
-	   #:make-lisp-interlan))
+	   #:make-lisp-interlan
+	   #:lex-line
+	   #:*linear-algebra-grammar*
+	   #:parse-with-lexer
+	   #:program
+	   #:make-program
+	   #:plus
+	   #:make-plus
+	   #:minus
+	   #:make-minus
+	   #:mul
+	   #:make-mul
+	   #:vec
+	   #:make-vec
+	   #:matrix
+	   #:make-matrix
+	   #:num
+	   #:make-num
+	   #:var
+	   #:make-var
+	   #:assignment
+	   #:make-assignment))
