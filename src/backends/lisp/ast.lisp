43182f03c7353a777bc6d198e049eca17caad515
Job <hj93@protonmail.com>
2022-10-20 19:39:32 -0700

    add intermediate language.

---
+++ src/backends/lisp/ast.lisp
@@ -0,0 +1,93 @@
+(in-package #:yotta)
+
+
+"""
+These are the nodes for the `lispIL` abstrax syntax tree -- i.e., intermediate language which
+transoforms the parse tree, a high level intermediate language.
+"""
+
+;;--------------------------------------------------------
+;;; lispIL ast nodes:
+;;--------------------------------------------------------
+
+(defstruct defvarlisp
+  "DEFVARLISP node."
+  var
+  exp)
+
+(defstruct vectorlisp
+  "VECTORLISP node."
+  dimension
+  elements)
+
+(defstruct matrixlisp
+  "MATRIXLISP node."
+  dimension
+  matrix)
+
+(defstruct numlisp
+  "NUMLISP node."
+  n)
+
+(defstruct looplisp
+  "LOOPLISP node."
+  i
+  n
+  exp)
+
+(defstruct (vectorsum (:include looplisp))
+  "VECTORSUM node."
+  leftexp
+  rightexp)
+
+(defstruct (vectorminus (:include looplisp))
+  "VECTOR-MINUS node."
+  leftexp
+  rightexp)
+
+(defstruct (matrixsum (:include looplisp))
+  "MATRIXSUM node."
+  leftexp
+  rightexp)
+
+(defstruct (matrixminus (:include looplisp))
+  "MATRIXMINUS node."
+  leftexp
+  rightexp)
+
+(defstruct prognlisp
+  "PROGNLISP node."
+  expressions)
+
+(defstruct setqlisp
+  "SETQLISP node."
+  var
+  exp)
+
+(defstruct setflisp
+  "SETFLISP node."
+  var
+  exp)
+
+(defstruct lengthlisp
+  "LENGTHLISP node."
+  exp)
+
+(defstruct areflisp
+  "AREFLISP node."
+  array
+  i)
+
+(defstruct sumlisp
+  "(primitive) SUMLISP node."
+  leftexp
+  rightexp)
+
+(defstruct minuslisp
+  "(primitive) MINUSLISP node."
+  leftexp
+  rightexp)
+
+;;todo: matrixmul, crossproduct, dotproduct
+
+
