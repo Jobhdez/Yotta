8edb4194de04f7c3ce15164081d0eae23d5cd8aa
Job <hj93@protonmail.com>
2022-10-24 20:21:50 -0700

    add matrix lispIL node test.

--- tests/lisp-IL-tests.lisp
+++ tests/lisp-IL-tests.lisp
@@ -12,6 +12,29 @@
 				     (yotta:MAKE-NUMLISP :N 4)
 				     (yotta:MAKE-NUMLISP :N 5)))))))
 
+(deftest test-lispIL-matrix ()
+  (is (equalp (yotta:make-lisp-interlan
+	       (yotta:parse-with-lexer
+	       (yotta:token-generator
+		(yotta:lex-line "[[4 5 6] [5 6 7]]"))
+	       yotta:*linear-algebra-grammar*))
+	      (list (yotta:make-matrixlisp
+		     :dimension (list 2 3)
+		     :matrix (list (yotta:MAKE-VECTORLISP
+				    :DIMENSION 3
+				    :ELEMENTS (list (yotta:MAKE-NUMLISP :N 4)
+				                    (yotta:MAKE-NUMLISP :N 5)
+				                    (yotta:MAKE-NUMLISP :N 6)))
+				    (yotta:MAKE-VECTORLISP
+				    :DIMENSION 3
+				    :ELEMENTS (list (yotta:MAKE-NUMLISP :N 5)
+				                    (yotta:MAKE-NUMLISP :N 6)
+				                    (yotta:MAKE-NUMLISP :N 7)))))))))
+				   
+
+
+
+
 (deftest test-lispIL-vecsum ()
   (is (equalp (yotta:make-lisp-interlan
 	       (yotta:parse-with-lexer
