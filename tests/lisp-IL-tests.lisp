69cad9084b4928666de1a5bd45dbd724992e32e4
Job <hj93@protonmail.com>
2022-10-24 23:50:56 -0700

    add matrix minus matrix test.

--- tests/lisp-IL-tests.lisp
+++ tests/lisp-IL-tests.lisp
@@ -188,3 +188,81 @@
                             :DIMENSION 3
                             :ELEMENTS (list (yotta:make-NUMLISP :N 12) (yotta:make-NUMLISP :N 13)
                                        (yotta:make-NUMLISP :N 14))))))))))
+
+(deftest test-lispIL-matrix-minus ()
+  (is (equalp (yotta:make-lisp-interlan
+	       (yotta:parse-with-lexer
+	       (yotta:token-generator
+		(yotta:lex-line "[[3 4 5] [6 7 8]] - [[9 10 11] [12 13 14]]"))
+	       yotta:*linear-algebra-grammar*))
+	      (list (yotta:make-MATRIXMINUS
+    :I (quote YOTTA-VAR::I)
+    :N 2
+    :EXP (yotta:make-LOOPLISP
+            :I (quote YOTTA-VAR::J)
+            :N 3
+            :EXP (yotta:make-SETFLISP
+                    :VAR (yotta:make-AREFLISP
+                            :ARRAY (quote YOTTA-VAR::NEWARRAY)
+                            :I (list (quote YOTTA-VAR::I) (quote YOTTA-VAR::J)))
+                    :EXP (yotta:make-MINUSLISP
+                            :LEFTEXP (yotta:make-AREFLISP
+                                        :ARRAY (yotta:make-MATRIXLISP
+                                                  :DIMENSION (list 2 3)
+                                                  :MATRIX (list (yotta:make-VECTORLISP
+                                                              :DIMENSION 3
+                                                              :ELEMENTS (list (yotta:make-NUMLISP
+                                                                            :N 3)
+                                                                         (yotta:make-NUMLISP
+                                                                            :N 4)
+                                                                         (yotta:make-NUMLISP
+                                                                            :N 5)))
+                                                           (yotta:make-VECTORLISP
+                                                              :DIMENSION 3
+                                                              :ELEMENTS (list (yotta:make-NUMLISP
+                                                                            :N 6)
+                                                                         (yotta:make-NUMLISP
+                                                                            :N 7)
+                                                                         (yotta:make-NUMLISP
+                                                                            :N 8)))))
+                                        :I (list (quote YOTTA-VAR::I) (quote YOTTA-VAR::J)))
+                            :RIGHTEXP (yotta:make-AREFLISP
+                                        :ARRAY (yotta:make-MATRIXLISP
+                                                  :DIMENSION (list 2 3)
+                                                  :MATRIX (list (yotta:make-VECTORLISP
+                                                              :DIMENSION 3
+                                                              :ELEMENTS (list (yotta:make-NUMLISP
+                                                                            :N 9)
+                                                                         (yotta:make-NUMLISP
+                                                                            :N 10)
+                                                                         (yotta:make-NUMLISP
+                                                                            :N 11)))
+                                                           (yotta:make-VECTORLISP
+                                                              :DIMENSION 3
+                                                              :ELEMENTS (list (yotta:make-NUMLISP
+                                                                            :N 12)
+                                                                         (yotta:make-NUMLISP
+                                                                            :N 13)
+                                                                         (yotta:make-NUMLISP
+                                                                            :N 14)))))
+                                        :I (list (quote YOTTA-VAR::I) (quote YOTTA-VAR::J))))))
+    :LEFTEXP (yotta:make-MATRIXLISP
+                :DIMENSION (list 2 3)
+                :MATRIX (list (yotta:make-VECTORLISP
+                            :DIMENSION 3
+                            :ELEMENTS (list (yotta:make-NUMLISP :N 3) (yotta:make-NUMLISP :N 4)
+                                       (yotta:make-NUMLISP :N 5)))
+                         (yotta:make-VECTORLISP
+                            :DIMENSION 3
+                            :ELEMENTS (list (yotta:make-NUMLISP :N 6) (yotta:make-NUMLISP :N 7)
+					    (yotta:make-NUMLISP :N 8)))))
+    :RIGHTEXP (yotta:make-MATRIXLISP
+                :DIMENSION (list 2 3)
+                :MATRIX (list (yotta:make-VECTORLISP
+                            :DIMENSION 3
+                            :ELEMENTS (list (yotta:make-NUMLISP :N 9) (yotta:make-NUMLISP :N 10)
+                                       (yotta:make-NUMLISP :N 11)))
+                         (yotta:make-VECTORLISP
+                            :DIMENSION 3
+                            :ELEMENTS (list (yotta:make-NUMLISP :N 12) (yotta:make-NUMLISP :N 13)
+                                       (yotta:make-NUMLISP :N 14))))))))))
